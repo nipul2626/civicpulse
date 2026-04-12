181cd2d66fe9576b22dcf60e3de6561c4a10d2ab
nipul2626 <nipulrathod34@gmail.com>
2026-04-12 18:11:32 +0530

    fix: index.js port fixed for producation

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const { startDailyJobs } = require('./jobs/dailyJobs');
 const { initRedis } = require('./services/cacheService');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // ── Middleware ────────────────────────────────────────────────────────────────
 app.use(compression()); // ✅ MUST be first (before routes)
