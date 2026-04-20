6696125a1467c2e452ed33f55a92e10984b00937
IshwariUgale-hub <ugaleishwari06@gmail.com>
2026-04-20 17:27:26 +0530

    Deleted unwanted file
    Co-authored-by: Nipul Rathod <nipulrathod34@gmail.com>

--- client/src/pages/ImpactPage.jsx
+++
@@ -1,6 +0,0 @@
-const ImpactPage = () => (
-    <div className="min-h-screen flex items-center justify-center">
-        <h1 className="gradient-text text-3xl font-display">Impact</h1>
-    </div>
-)
-export default ImpactPage
